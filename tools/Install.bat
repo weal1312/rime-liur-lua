886fb5739e4c0417e812948b30ba49292d9e1eb9
Halley Tsai <hftsai256@gmail.com>
2020-04-12 15:04:36 -0400

    separate copy logic

--- tools/Install.bat
+++ tools/Install.bat
@@ -1,5 +1,7 @@
 del %AppData%\Rime\*.* /Q
 rmdir %AppData%\RIme\opencc /S /Q
-xcopy ..\src\*.* /S %AppData%\Rime\
-rename %AppData%\Rime\default.custom.in %AppData%\Rime\default.custom.yaml
+xcopy ..\src\*.yaml /S %AppData%\Rime\
+xcopy ..\src\*.lua  /S %AppData%\Rime\
+xcopy ..\src\opencc /S %AppData%\Rime\
+xcopy ..\src\default.custom.in %AppData%\Rime\default.custom.yaml
 "C:\Program Files (x86)\Rime\weasel-0.13.0\WeaselDeployer.exe" /deploy
