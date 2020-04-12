6696ac36fa7a9bc50d54b74c0ffa8143e485afc7
Halley Tsai <hftsai256@gmail.com>
2020-04-12 14:22:25 -0400

    initial windows install support, help/tester wanted

--- tools/Install.bat
+++ tools/Install.bat
@@ -1,4 +1,5 @@
 del %AppData%\Rime\*.* /Q
 rmdir %AppData%\RIme\opencc /S /Q
-xcopy .\Rime\*.* /S %AppData%\Rime\
+xcopy ..\src\*.* /S %AppData%\Rime\
+rename %AppData%\Rime\default.custom.in %AppData%\Rime\default.custom.yaml
 "C:\Program Files (x86)\Rime\weasel-0.13.0\WeaselDeployer.exe" /deploy
